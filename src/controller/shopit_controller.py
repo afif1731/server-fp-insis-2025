1932331d422fae1a5905cbcd4f5032b784ad5d94
afif1731 <afifsdi@gmail.com>
2025-06-18 15:50:29 +0700

    feat: shopit transaction

--- src/controller/shopit_controller.py
+++ src/controller/shopit_controller.py
@@ -1,11 +1,13 @@
 import paho.mqtt.client as mqtt
 import json
 
+from src.service.live_balance_history import send_live_update_balance
 import src.service.shopit_services as productService
 
 from src.middleware.validator import do_validate
 from src.utils.split_topic import topic_splitter
-from src.validator.shopit_validator import getProductByIdValidator
+from src.utils.validator_helper import validate_email
+from src.validator.shopit_validator import getProductByIdValidator, buyProductValidator
 from src.middleware.custom_response import CustomResponse
 from src.middleware.custom_error import CustomError
 
@@ -33,6 +35,39 @@ async def getProductByIdController(client: mqtt.Client, userdata, message: mqtt.
         response = CustomResponse(200, 'sukses mendapatkan detail produk', result)
 
         client.publish(topic=callback_topic, payload=json.dumps(response.JSON()))
+    except CustomError as err:
+        print(err.JSON())
+        client.publish(topic=callback_topic, payload=json.dumps(err.JSON()))
+
+async def buyProductController(client: mqtt.Client, userdata, message: mqtt.MQTTMessage):
+    try:
+        topic_parts = topic_splitter(message.topic)
+        callback_topic = f'{topic_parts[0]}/{topic_parts[1]}/shopit/buy/response'
+
+        data = do_validate(buyProductValidator, json.loads(message.payload))
+        email = validate_email(data['buyer_email'])
+
+        result = await productService.buyProduct(
+            user_class=topic_parts[0],
+            user_group=topic_parts[1],
+            buyer_email=email,
+            payment_method=data['payment_method'],
+            product_id=data['product_id'],
+            quantity=data['quantity']
+        )
+        response = CustomResponse(200, 'transaksi ShopIT berhasil', result['data'])
+        
+        client.publish(topic=callback_topic, payload=json.dumps(response.JSON()))
+        send_live_update_balance(
+            client=client,
+            user_class=topic_parts[0],
+            user_group=topic_parts[1],
+            payment_method=data['payment_method'],
+            transaction_type='PURCHASE',
+            amount=-result['data']['total_price'],
+            updated_balance=result['data']['current_balance'],
+            message=result['message']
+            )
     except CustomError as err:
         print(err.JSON())
         client.publish(topic=callback_topic, payload=json.dumps(err.JSON()))
--- src/service/shopit_services.py
+++ src/service/shopit_services.py
@@ -42,3 +42,97 @@ async def getProductByIdService(product_id: str):
         'price': product.price,
         'quantity': product.quantity
     }
+
+async def buyProduct(user_class: str, user_group: str, buyer_email: str, payment_method: str, product_id: str, quantity: int):
+    isUserExist = await prisma.accounts.find_first(
+        where={ 'email': buyer_email }
+    )
+
+    if isUserExist is None:
+        raise CustomError(404, 'user tidak ditemukan')
+    
+    if isUserExist.role == 'USER' and buyer_email != f'insys-{user_class}-{user_group}@bankit.com':
+        raise CustomError(403, 'user tidak dapat mengakses data ini')
+
+    isUserWalletExist = await prisma.userwallets.find_first(
+        where={'AND': [
+            {'account_id': isUserExist.id},
+            {'payment_method_slug': payment_method}
+        ]},
+        include={'payment_method': True}
+    )
+
+    if isUserWalletExist is None:
+        raise CustomError(404, 'E-Wallet user tidak ditemukan')
+    
+    product = await prisma.shopproducts.find_first(
+        where={'id': product_id}
+    )
+    
+    if product is None:
+        raise CustomError(404, 'produk tidak ditemukan')
+    
+    if quantity <= 0:
+        raise CustomError(422, 'jumlah pembelian minimal 1')
+
+    if product.quantity < quantity:
+        raise CustomError(400, 'inventaris produk tidak mencukupi')
+    
+    total_price = product.price * quantity
+
+    if isUserWalletExist.balance < total_price:
+        raise CustomError(400, 'uang E-Wallet tidak cukup')
+    
+    # purchase_msg = f'{isUserExist.name} membeli {quantity} buah {product.name} diShopIT via {isUserWalletExist.payment_method.payment_name}'
+    purchase_msg_simple = f'Pembelian {quantity} buah {product.name} di ShopIT'
+
+    # Kurangi Produk
+    updatedProduct = await prisma.shopproducts.update(
+        where={'id': product.id},
+        data={'quantity': product.quantity - quantity}
+    )
+
+    if updatedProduct.quantity == 0:
+        await prisma.shopproducts.update(
+            where={'id': product.id},
+            data={'quantity': 999}
+        )
+    
+    # Kurangi Duit User
+    updatedUserWallet = await prisma.userwallets.update(
+        where={'id': isUserWalletExist.id},
+        data={'balance': isUserWalletExist.balance - total_price}
+    )
+
+    # Buat Hasil Transaksi
+    newPurchase = await prisma.shoptransactions.create(
+        data={
+            'product_id': product_id,
+            'user_wallet_id': isUserWalletExist.id,
+            'item_quantity': quantity,
+            'total_price': total_price
+        }
+    )
+
+    # Catat juga di Wallet Transaction
+    newWalletTransaction = await prisma.wallettransactions.create(
+        data={
+            'user_wallet_id': isUserWalletExist.id,
+            'balance_change': -total_price,
+            'transaction_type': 'PURCHASE',
+            'description': purchase_msg_simple
+        }
+    )
+
+    return{
+        'data': {
+            'product_id': product.id,
+            'purchase_id': newPurchase.id,
+            'wallet_transaction_id': newWalletTransaction.id,
+            'quantity': quantity,
+            'total_price': total_price,
+            'discount': 0,
+            'current_balance': updatedUserWallet.balance
+        },
+        'message': purchase_msg_simple
+    }
